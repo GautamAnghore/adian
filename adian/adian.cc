e07a1fb66205bc969c647e60a95c6a6684d8dc40
GautamAnghore <gautam.execute@gmail.com>
2015-05-09 20:49:25 +0530

    + packet binding to tcl interface

---
+++ adian/adian.cc
@@ -0,0 +1,17 @@
+//binding our packet to TCL interface
+//for that we use offset
+
+int adian_pkt::offset_;
+
+//Our header class inherits Packet's header class
+static class AdianHeaderClass : public PacketHeaderClass {
+
+public:
+	//constructor
+	//we are passing values to the parent class constructor 
+	AdianHeaderClass() : PacketHeaderClass("PacketHeader/Protoname", sizeof(hdr_adian_pkt)) {
+		//Q: don't know what this bind_offset does
+		bind_offset(&hdr_adian_pkt::offset_);
+	}
+
+}class_rtProtoAdian_hdr;
