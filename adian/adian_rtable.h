5ee5b79b44b308245f0357e7340a78609efce09f
TavneetSingh <tavneet.singh2509@gmail.com>
2015-05-10 05:55:54 +0530

    adding files to master branch

---
+++ adian/adian_rtable.h
@@ -0,0 +1,22 @@
+#ifndef __protoname_rtable_h__
+#define __protoname_rtable_h__
+#include <trace.h>
+#include <map>			//We use a hash table (map) as the storage structure.
+#include<common/ip.h>
+#include<common/packet.h>
+/*For each entry in routing table one might want to store destination addresses, next hop addresses, distances or cost
+associated to the routes, sequence numbers, lifetimes.*/
+
+ typedef std::map<nsaddr_t, nsaddr_t> rtable_t;
+ class protoname_rtable {
+ rtable_t rt_;
+ public:
+  protoname_rtable();
+  void print(Trace*);
+  void clear();
+  void rm_entry(nsaddr_t);
+  void add_entry(nsaddr_t, nsaddr_t);
+  nsaddr_t lookup(nsaddr_t);
+  u_int32_t
+ };
+ #endif
