822cf5e2c1d2e39ae1a996150b12f065d033a781
Harshit Thukral <H.thakral369@gmail.com>
2015-05-11 02:18:04 +0530

    code cleanup: fix naming and identation problems in adian_rtable.cc

--- adian/adian_rtable.cc
+++ adian/adian_rtable.cc
@@ -1,47 +1,49 @@
-ADIAN_rtable::ADIAN_rtable() { }
+
+adian_rtable::adian_rtable() { }
 
 //The print() function will dump the contents of the node’s routing table to the trace file.
- void ADIAN_rtable::print(Trace* out) {
- sprintf(out->pt_->buffer(), "P\tdest\tnext");
- out->pt_->dump();
- for (rtable_t::iterator it = rt_.begin(); it != rt_.end(); it++) 
-  {
-   sprintf(out->pt_->buffer(), "P\t%d\t%d",(*it).first,(*it).second);
-   out->pt_->dump();
-  }
+void adian_rtable::print(Trace* out) {
+	sprintf(out->pt_->buffer(), "P\tdest\tnext");
+	out->pt_->dump();
+	for (rtable_t::iterator it = rt_.begin(); it != rt_.end(); it++) 
+  	{
+   		sprintf(out->pt_->buffer(), "P\t%d\t%d",(*it).first,(*it).second);
+   		out->pt_->dump();
+  	}
  }
 
 
 //function removes all entries in routing table.
- void ADIAN_rtable::clear() {
-  rt_.clear();
- }
+void adian_rtable::clear() {
+ 	rt_.clear();
+}
 
 
 //To remove an entry given its destination address we implement the rm entry() function.
- void ADIAN_rtable::rm_entry(nsaddr_t dest) {
- rt_.erase(dest);
- }
+void adian_rtable::rm_entry(nsaddr_t dest) {
+	rt_.erase(dest);
+}
 
 
 //add_entry() is used to add a new entry in the routing table given its destination and next hop addresses.
- void ADIAN_rtable::add_entry(nsaddr_t dest, nsaddr_t next) {
- rt_[dest] = next;
- }
+void adian_rtable::add_entry(nsaddr_t dest, nsaddr_t next) {
+	rt_[dest] = next;
+}
 
 
 /*definition of lookup().
 Lookup() returns the next hop address of an entry given its destination address. If such an entry doesn’t exist, (that is, there is no route for that destination) the function returns IP BROADCAST. Of course we include common/ip.h in order to use this constant.*/
- nsaddr_t ADIAN_rtable::lookup(nsaddr_t dest) {
- rtable_t::iterator it = rt_.find(dest);
- if (it == rt_.end())
- return IP_BROADCAST;
- else
-  return (*it).second;
+nsaddr_t adian_rtable::lookup(nsaddr_t dest) {
+	rtable_t::iterator it = rt_.find(dest);
+
+	if (it == rt_.end())
+ 		return IP_BROADCAST;
+ 	else
+  		return (*it).second;
  }
 
 
 //size() returns the number of entries in the routing table.
- u_int32_t  ADIAN_rtable::size() {
- return rt_.size();
+u_int32_t  adian_rtable::size() {
+	return rt_.size();
  }
